8e87b3216e73559ddbe91d92dab89bac176c3849
Rafael <rafaelsrios12@gmail.com>
2022-03-29 15:32:56 -0300

    Publicação do pacote no Pypi

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro"
+PACKAGE = "libpythonpro-rafael"
 NAME = PACKAGE
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Rafael Rios"
