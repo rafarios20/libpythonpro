f61c62b1614313354bf2b33df6dcd1d98382ea28
Rafael <rafaelsrios12@gmail.com>
2022-04-04 15:15:43 -0300

    Começo da implantação do banco de dados

---
+++ libpythonpro/tests/teste_spam/test_usuarios.py
@@ -0,0 +1,20 @@
+def test_salvar_usuario():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuario = Usuario(nome='Rafael')
+    sessao.salvar(usuario)
+    assert isinstance(usuario.id, int)
+    sessao.roll_back()
+    sessao.fechar
+    conexao.fechar
+
+def test_listar_usuarios():
+    conexao = Conexao()
+    sessao = conexao.gerar_sessao()
+    usuarios = [Usuario(nome='Rafael'), Usuario(nome='Luciano')]
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    assert usuario==sessao.listar()
+    sessao.roll_back()
+    sessao.fechar()
+    conexao.fechar()
